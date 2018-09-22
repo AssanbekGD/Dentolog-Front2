fa152d75508d890c490b7bb4b3c13b6825af42c4
AssanbekGD <assanbek.g.d@gmail.com>
2018-09-22 22:45:11 +0600

    resource + employee + mongo

--- imports/ui/resources/employee/Employee.jsx
+++ imports/ui/resources/employee/Employee.jsx
@@ -1,27 +1,81 @@
 import React, { Component } from 'react';
 import { Item,Form, Image,Header,Button,Segment,Table,Grid } from 'semantic-ui-react';
+import {Employees} from '/imports/api/Employees.js';
 
+const renderEmployee = function (renderEmployee) {
+  return renderEmployee.map(function (eEmployee) {
+    return
+
+  });
+};
+
+const handleSubmit = function (e) {
+  let employeeSurname = e.target.employeeSurname.value;
+  let employeeName = e.target.employeeName.value;
+  let employeefName = e.target.employeefName.value;
+  let employeeHomePhone = e.target.employeeHomePhone.value;
+  let employeeMobilePhone = e.target.employeeMobilePhone.value;
+  let employeeEmail = e.target.employeeEmail.value;
+  let employeeCity = e.target.employeeCity.value;
+  let employeeStreet = e.target.employeeStreet.value;
+  let employeeHome = e.target.employeeHome.value;
+    let employeeFlat = e.target.employeeFlat.value;
+  e.preventDefault();
+
+  if (employeeName) {
+    e.target.employeeSurname.value = '';
+      e.target.employeeName.value = '';
+        e.target.employeefName.value = '';
+          e.target.employeeHomePhone.value = '';
+            e.target.employeeMobilePhone.value = '';
+              e.target.employeeEmail.value = '';
+                e.target.employeeCity.value = '';
+                  e.target.employeeStreet.value = '';
+                    e.target.employeeHome.value = '';
+                      e.target.employeeFlat.value = '';
+
+    Employees.insert({
+      surname: employeeSurname,
+      name: employeeName,
+      Fname: employeefName,
+      homePhone:employeeHomePhone,
+      mobilePhone:employeeMobilePhone,
+      email:employeeEmail,
+      city:employeeCity,
+      street:employeeStreet,
+      home:employeeHome,
+      flat:employeeFlat
+
+    });
+  }
+};
 
 
 export default class Employee extends Component {
 
 
   render() {
+    Tracker.autorun(function(){
+      let employees = Employees.find().fetch();
+    });
     return <div>
 
-<Form>
+<Form onSubmit={handleSubmit}>
   <Header as="h5">ФИО</Header>
   <hr/>
   <Form.Group>
     <Form.Input
+      name="employeeSurname"
    inline
    label="Фамилия"
     />
     <Form.Input
+      name="employeeName"
      inline
      label="Имя"
       />
     <Form.Input
+      name="employeefName"
      inline
      label="Отчество"
       />
@@ -31,14 +85,17 @@ export default class Employee extends Component {
  <hr/>
  <Form.Group>
    <Form.Input
+     name="employeeHomePhone"
   inline
   label="Домашний телефон"
    />
    <Form.Input
+     name="employeeMobilePhone"
     inline
     label="Сотовый телефон"
      />
    <Form.Input
+     name="employeeEmail"
     inline
     label ="Email"
      />
@@ -47,18 +104,22 @@ export default class Employee extends Component {
 <hr/>
 <Form.Group>
   <Form.Input
+    name="employeeCity"
  inline
  label="Город"
   />
   <Form.Input
+    name="employeeStreet"
    inline
    label="Улица"
     />
   <Form.Input
+    name="employeeHome"
    inline
    label="Дом"
     />
       <Form.Input
+        name="employeeFlat"
        inline
        label="Квартира"
         />
@@ -70,7 +131,7 @@ export default class Employee extends Component {
  placeholder="Профессия"
   />
   <Form.Input
-    width={200}
+
    inline
    label="Коментарий"
     />
--- server/main.js
+++ server/main.js
@@ -4,6 +4,7 @@ import {Diagnos} from '/imports/api/Diagnos.js';
 import {Services} from '/imports/api/Services.js';
 import {Mkb} from '/imports/api/Mkb.js';
 import {ToothStatus} from '/imports/api/ToothStatus.js';
+import {Employees} from '/imports/api/Employees.js';
 
 Meteor.startup(() => {
 /*  Material.insert({
